379884172dc70b3e0983f8829dc19fd6c8497dee
Zangar python dev. <zangar12334@gmail.com>
2025-08-26 23:43:56 +0300

    heheh

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CreateBlog
+from .views import CreateBlog,CreateBlocks,BlogGet
 
 urlpatterns = [
-    path("private/",CreateBlog.as_view())
+    path("private/",CreateBlog.as_view()),
+    path("<int:pk>/",BlogGet.as_view()),
+    path("<int:pk>/blocks/",CreateBlocks.as_view())
 ]
--- news/views.py
+++ news/views.py
@@ -9,7 +9,7 @@ from rest_framework.views import APIView
 class CreateBlog(APIView):
     permission_classes = [IsAuthenticated]
     
-    def post(self,request=Request):
+    def post(self,request:Request):
         data = {
             "author":request.user.id,
             "header":request.data["header"],
@@ -24,3 +24,45 @@ class CreateBlog(APIView):
         private_blogs = Blog.objects.filter(public=False,author=request.user)
         serializer = BlogSerializer(private_blogs,many=True)
         return Response(serializer.data)
+
+class CreateBlocks(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self,request:Request,pk):
+        blog = get_object_or_404(Blog,pk=pk)
+        if blog.author != request.user:
+            return Response("У вас нет доступа к этому блогу")
+        if blog.public == True:
+            return Response("Он уже опубликован.Ее нельзя изменить")
+        
+        data = {
+            "header":request.data['header'],
+            "title":request.data['title'],
+            "image":request.data['image'],
+            "blog":blog.id
+        }
+        serializer = BlocksSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Created!")
+        return Response(serializer.errors)
+        # return Response("Test")
+class BlogGet(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request:Request,pk):
+        blog = get_object_or_404(Blog,pk=pk)
+        if blog.public == False:
+            if blog.author != request.user:
+                return Response(data={"Доступ запрещен"})
+        blocks = blog.blocks.all()
+        blog_serializer = BlogSerializer(blog)
+        blocks_serializer = BlocksSerializer(blocks,many=True)
+        return Response(
+            {
+                "Blog":blog_serializer.data,
+                "Blocks":blocks_serializer.data
+            }
+        )
+    
+        
+         
