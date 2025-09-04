ce6b484580e63282cccb83c823145253e78312fe
Zangar python dev. <zangar12334@gmail.com>
2025-09-04 23:13:43 +0300

    Parsed create
    Parser is working great.
    yeah

--- parse.py
+++ parse.py
@@ -19,27 +19,35 @@ if response.status_code == 200:
     # headlines = soup.find_all("temperature-value")
     headlines = soup.find_all("div",class_="value")
     
+    obj = []
+    
     for d in day:
         day_ = d.find("div",class_="day")
         date_ = d.find("div",class_="date")
         if day_ and date_:  # проверка, что теги найдены
-            print(f"{day_.get_text(strip=True)}")
-            print(f"{date_.get_text(strip=True)}")
+            data = {
+                "day":day_.get_text(strip=True),
+                "date":date_.get_text(strip=True)
+            }
+            obj.append(data)
+            # print(f"{day_.get_text(strip=True)}")
+            # print(f"{date_.get_text(strip=True)}")
+    # print(obj)
     
-    for h in headlines:
+    for i,h in enumerate(headlines[:10],0):
         max_v = h.find("div",class_="maxt")
         min_v = h.find("div",class_="mint")
         
         if max_v and min_v:
             max_value = max_v.find("temperature-value")
             min_value = min_v.find("temperature-value")
+            
             if max_value and min_value:
-                
-                print("{")
-                print(f"max:{max_value.get("value","")}")
-                print(f"min:{min_value.get("value","")}")
-                print("}")
-        # print(h.get("value"))
+                obj[i]["max"] = max_value.get("value","")
+                obj[i]["min"] = min_value.get("value","")
+                print(obj[i])
+    RESULT = obj  
+    
 else:
     print("errors:",response.status_code)
     
